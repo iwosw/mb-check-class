41e535997ce4264ca4908b2fa19fafc745e986aa
pozdn.r.a <kaisergrobe@gmail.com>
2026-04-15 13:35:37 +0700

    revert(21): pivot bannerlord -> bannermod -- feat(21-02): move bootstrap ownership into bannerlord packages

--- src/test/java/com/talhanation/bannermod/BannerModIntegratedRuntimeSmokeTest.java
+++ src/test/java/com/talhanation/bannermod/BannerModIntegratedRuntimeSmokeTest.java
@@ -1,8 +1,7 @@
 package com.talhanation.bannermod;
 
-import com.talhanation.bannerlord.bootstrap.BannerlordMain;
-import com.talhanation.bannerlord.network.military.RecruitsNetworkRegistrar;
 import com.talhanation.recruits.Main;
+import com.talhanation.recruits.network.RecruitsNetworkRegistrar;
 import com.talhanation.workers.WorkersRuntime;
 import com.talhanation.workers.WorkersSubsystem;
 import org.junit.jupiter.api.Test;
@@ -17,8 +16,7 @@ class BannerModIntegratedRuntimeSmokeTest {
         RecruitsNetworkRegistrar recruitsNetworkRegistrar = new RecruitsNetworkRegistrar();
         WorkersSubsystem workersSubsystem = new WorkersSubsystem();
 
-        assertEquals("bannermod", BannerlordMain.MOD_ID);
-        assertEquals(BannerlordMain.MOD_ID, Main.MOD_ID);
+        assertEquals("bannermod", Main.MOD_ID);
         assertEquals(Main.MOD_ID, WorkersRuntime.modId());
         assertEquals(recruitsNetworkRegistrar.orderedMessageTypes().size(), WorkersRuntime.networkIdOffset());
         assertEquals(20, workersSubsystem.networkMessageCount());
