5558c85d1a5c652bf37d70bab5a6fe8b49c6bd47
pozdn.r.a <kaisergrobe@gmail.com>
2026-04-28 22:58:25 +0700

    Refactor recruit renderer base

--- src/main/java/com/talhanation/bannermod/client/military/render/RecruitHumanRenderer.java
+++ src/main/java/com/talhanation/bannermod/client/military/render/RecruitHumanRenderer.java
@@ -1,7 +1,5 @@
 package com.talhanation.bannermod.client.military.render;
-import com.mojang.blaze3d.vertex.PoseStack;
 import com.talhanation.bannermod.bootstrap.BannerModMain;
-import com.talhanation.bannermod.client.military.events.ClientEvent;
 import com.talhanation.bannermod.client.military.models.RecruitHumanModel;
 import com.talhanation.bannermod.client.military.render.layer.RecruitHumanBiomeLayer;
 import com.talhanation.bannermod.client.military.render.layer.RecruitHumanCompanionLayer;
@@ -9,26 +7,15 @@ import com.talhanation.bannermod.client.military.render.layer.RecruitHumanTeamCo
 import com.talhanation.bannermod.client.military.render.layer.RecruitLodArmorLayer;
 import com.talhanation.bannermod.client.military.render.layer.RecruitLodCustomHeadLayer;
 import com.talhanation.bannermod.client.military.render.layer.RecruitLodItemInHandLayer;
-import com.talhanation.bannermod.compat.IWeapon;
 import com.talhanation.bannermod.entity.military.AbstractRecruitEntity;
-import com.talhanation.bannermod.entity.military.CrossBowmanEntity;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.layers.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.ModelLayers;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.entity.MobRenderer;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.HumanoidArm;
-import net.minecraft.world.item.CrossbowItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.UseAnim;
 
-public class RecruitHumanRenderer extends MobRenderer<AbstractRecruitEntity, HumanoidModel<AbstractRecruitEntity>> {
+public class RecruitHumanRenderer extends AbstractRecruitRenderer<HumanoidModel<AbstractRecruitEntity>> {
 
     private static final ResourceLocation[] TEXTURE = {
             ResourceLocation.fromNamespaceAndPath(BannerModMain.MOD_ID, "textures/entity/human/human_0.png"),
@@ -76,89 +63,10 @@ public class RecruitHumanRenderer extends MobRenderer<AbstractRecruitEntity, Hum
     }
 
 
-    public void render(AbstractRecruitEntity recruit, float p_117789_, float p_117790_, PoseStack p_117791_, MultiBufferSource p_117792_, int p_117793_) {
-        long poseStart = RecruitRenderProfiling.start();
-        this.setModelProperties(recruit);
-        RecruitRenderProfiling.duration("animation_pose", poseStart);
-        RecruitRenderProfiling.beginNormalRender();
-        long renderStart = RecruitRenderProfiling.start();
-        super.render(recruit, p_117789_, p_117790_, p_117791_, p_117792_, p_117793_);
-        RecruitRenderProfiling.endNormalRender(renderStart);
-    }
-
     @Override
-    protected boolean shouldShowName(AbstractRecruitEntity recruit) {
-        boolean showName = RecruitRenderLod.shouldRenderName(recruit) && super.shouldShowName(recruit);
-        if (showName) {
-            RecruitRenderProfiling.increment("nameplates.visible");
-        } else {
-            RecruitRenderProfiling.skipped("nameplates");
-        }
-        return showName;
-    }
-
-    @Override
-    protected void renderNameTag(AbstractRecruitEntity recruit, Component displayName, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, float partialTick) {
-        long start = RecruitRenderProfiling.start();
-        super.renderNameTag(recruit, displayName, poseStack, bufferSource, packedLight, partialTick);
-        RecruitRenderProfiling.duration("nameplates", start);
-    }
-
-    private void setModelProperties(AbstractRecruitEntity recruit) {
-        HumanoidModel<AbstractRecruitEntity> model = this.getModel();
-
-        model.setAllVisible(true);
-        model.crouching = recruit.isCrouching();
-        HumanoidModel.ArmPose humanoidmodel$armpose = getArmPose(recruit, InteractionHand.MAIN_HAND);
-        HumanoidModel.ArmPose humanoidmodel$armpose1 = getArmPose(recruit, InteractionHand.OFF_HAND);
-        if (humanoidmodel$armpose.isTwoHanded()) {
-            humanoidmodel$armpose1 = recruit.getOffhandItem().isEmpty() ? HumanoidModel.ArmPose.EMPTY : HumanoidModel.ArmPose.ITEM;
-        }
-        if (recruit.getMainArm() == HumanoidArm.RIGHT) {
-            model.rightArmPose = humanoidmodel$armpose;
-            model.leftArmPose = humanoidmodel$armpose1;
-        } else {
-            model.rightArmPose = humanoidmodel$armpose1;
-            model.leftArmPose = humanoidmodel$armpose;
-        }
-    }
-
-    private static HumanoidModel.ArmPose getArmPose(AbstractRecruitEntity recruit, InteractionHand hand) {
-        ItemStack itemstack = recruit.getItemInHand(hand);
-        boolean isMusket = IWeapon.isMusketModWeapon(itemstack) && (recruit instanceof CrossBowmanEntity crossBowman)  && crossBowman.isAggressive();
-        if (itemstack.isEmpty()) {
-            return HumanoidModel.ArmPose.EMPTY;
-        } else {
-            if (recruit.getUsedItemHand() == hand && recruit.getUseItemRemainingTicks() > 0) {
-                UseAnim useanim = itemstack.getUseAnimation();
-                if (useanim == UseAnim.BLOCK) {
-                    return HumanoidModel.ArmPose.BLOCK;
-                }
-
-                if (useanim == UseAnim.BOW) {
-                    return HumanoidModel.ArmPose.BOW_AND_ARROW;
-                }
-
-                if (useanim == UseAnim.SPEAR) {
-                    return HumanoidModel.ArmPose.THROW_SPEAR;
-                }
-
-                if (useanim == UseAnim.CROSSBOW && hand == recruit.getUsedItemHand() || isMusket) {
-                    return HumanoidModel.ArmPose.CROSSBOW_CHARGE;
-                }
-
-                if (useanim == UseAnim.SPYGLASS) {
-                    return HumanoidModel.ArmPose.SPYGLASS;
-                }
-            } else if (!recruit.swinging && itemstack.is(Items.CROSSBOW) && CrossbowItem.isCharged(itemstack) || isMusket) {
-                return HumanoidModel.ArmPose.CROSSBOW_HOLD;
-            }
-
-            HumanoidModel.ArmPose forgeArmPose = net.neoforged.neoforge.client.extensions.common.IClientItemExtensions.of(itemstack).getArmPose(recruit, hand, itemstack);
-            if (forgeArmPose != null) return forgeArmPose;
-
-            return HumanoidModel.ArmPose.ITEM;
-        }
+    protected HumanoidModel.ArmPose getItemFallbackArmPose(AbstractRecruitEntity recruit, InteractionHand hand, ItemStack itemStack) {
+        HumanoidModel.ArmPose forgeArmPose = net.neoforged.neoforge.client.extensions.common.IClientItemExtensions.of(itemStack).getArmPose(recruit, hand, itemStack);
+        return forgeArmPose != null ? forgeArmPose : HumanoidModel.ArmPose.ITEM;
     }
 
 }
--- src/main/java/com/talhanation/bannermod/client/military/render/RecruitVillagerRenderer.java
+++ src/main/java/com/talhanation/bannermod/client/military/render/RecruitVillagerRenderer.java
@@ -10,27 +10,12 @@ import com.talhanation.bannermod.client.military.render.layer.RecruitVillagerTea
 import com.talhanation.bannermod.client.military.render.layer.RecruitLodArmorLayer;
 import com.talhanation.bannermod.client.military.render.layer.RecruitLodItemInHandLayer;
 import com.talhanation.bannermod.client.military.render.layer.VillagerRecruitCustomHeadLayer;
-import com.talhanation.bannermod.compat.IWeapon;
-import com.talhanation.bannermod.entity.military.AbstractInventoryEntity;
 import com.talhanation.bannermod.entity.military.AbstractRecruitEntity;
-import com.talhanation.bannermod.entity.military.CrossBowmanEntity;
-import com.talhanation.bannermod.entity.military.VillagerNobleEntity;
 import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.MobRenderer;
-import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
-import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.entity.HumanoidArm;
-import net.minecraft.world.item.CrossbowItem;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.UseAnim;
 
-public class RecruitVillagerRenderer extends MobRenderer<AbstractRecruitEntity, HumanoidModel<AbstractRecruitEntity>> {
+public class RecruitVillagerRenderer extends AbstractRecruitRenderer<HumanoidModel<AbstractRecruitEntity>> {
 
     private static final ResourceLocation[] TEXTURE = {
             ResourceLocation.fromNamespaceAndPath(BannerModMain.MOD_ID, "textures/entity/villager/villager_1.png"),
@@ -55,92 +40,7 @@ public class RecruitVillagerRenderer extends MobRenderer<AbstractRecruitEntity, 
     }
 
     @Override
-    public void render(AbstractRecruitEntity entityIn, float entityYaw, float partialTicks, PoseStack matrixStackIn, MultiBufferSource bufferIn, int packedLightIn) {
-        long poseStart = RecruitRenderProfiling.start();
-        this.setModelVisibilities(entityIn);
-        RecruitRenderProfiling.duration("animation_pose", poseStart);
-        RecruitRenderProfiling.beginNormalRender();
-        long renderStart = RecruitRenderProfiling.start();
-        super.render(entityIn, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn);
-        RecruitRenderProfiling.endNormalRender(renderStart);
-    }
-
-    @Override
-    protected boolean shouldShowName(AbstractRecruitEntity recruit) {
-        boolean showName = RecruitRenderLod.shouldRenderName(recruit) && super.shouldShowName(recruit);
-        if (showName) {
-            RecruitRenderProfiling.increment("nameplates.visible");
-        } else {
-            RecruitRenderProfiling.skipped("nameplates");
-        }
-        return showName;
-    }
-
-    @Override
-    protected void renderNameTag(AbstractRecruitEntity recruit, Component displayName, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, float partialTick) {
-        long start = RecruitRenderProfiling.start();
-        super.renderNameTag(recruit, displayName, poseStack, bufferSource, packedLight, partialTick);
-        RecruitRenderProfiling.duration("nameplates", start);
-    }
-
-    //PlayerRenderer
-    private void setModelVisibilities(AbstractInventoryEntity recruitEntity) {
-        HumanoidModel<AbstractRecruitEntity> model = this.getModel();
-        model.setAllVisible(true);
-        model.crouching = recruitEntity.isCrouching();
-        model.setAllVisible(true);
-        model.hat.visible = true;
-        HumanoidModel.ArmPose humanoidmodel$armpose = getArmPose(recruitEntity, InteractionHand.MAIN_HAND);
-        HumanoidModel.ArmPose humanoidmodel$armpose1 = getArmPose(recruitEntity, InteractionHand.OFF_HAND);
-        if (humanoidmodel$armpose.isTwoHanded()) {
-            humanoidmodel$armpose1 = recruitEntity.getOffhandItem().isEmpty() ? HumanoidModel.ArmPose.EMPTY : HumanoidModel.ArmPose.ITEM;
-        }
-
-        if (recruitEntity.getMainArm() == HumanoidArm.RIGHT) {
-            model.rightArmPose = humanoidmodel$armpose;
-            model.leftArmPose = humanoidmodel$armpose1;
-        } else {
-            model.rightArmPose = humanoidmodel$armpose1;
-            model.leftArmPose = humanoidmodel$armpose;
-        }
-    }
-
-    @Override
     protected void scale(AbstractRecruitEntity entitylivingbaseIn, PoseStack matrixStackIn, float partialTickTime) {
         matrixStackIn.scale(0.9375F, 0.9375F, 0.9375F);
-    }
-
-    private static HumanoidModel.ArmPose getArmPose(AbstractInventoryEntity recruit, InteractionHand hand) {
-        ItemStack itemstack = recruit.getItemInHand(hand);
-        boolean isMusket = IWeapon.isMusketModWeapon(itemstack) && (recruit instanceof CrossBowmanEntity crossBowman)  && crossBowman.isAggressive();
-        if (itemstack.isEmpty()) {
-            return HumanoidModel.ArmPose.EMPTY;
-        } else {
-            if (recruit.getUsedItemHand() == hand && recruit.getUseItemRemainingTicks() > 0) {
-                UseAnim useanim = itemstack.getUseAnimation();
-                if (useanim == UseAnim.BLOCK) {
-                    return HumanoidModel.ArmPose.BLOCK;
-                }
-
-                if (useanim == UseAnim.BOW) {
-                    return HumanoidModel.ArmPose.BOW_AND_ARROW;
-                }
-
-                if (useanim == UseAnim.SPEAR) {
-                    return HumanoidModel.ArmPose.THROW_SPEAR;
-                }
-
-                if (useanim == UseAnim.CROSSBOW && hand == recruit.getUsedItemHand() || isMusket) {
-                    return HumanoidModel.ArmPose.CROSSBOW_CHARGE;
-                }
-
-                if (useanim == UseAnim.SPYGLASS) {
-                    return HumanoidModel.ArmPose.SPYGLASS;
-                }
-            } else if (!recruit.swinging && itemstack.is(Items.CROSSBOW) && CrossbowItem.isCharged(itemstack) || isMusket) {
-                return HumanoidModel.ArmPose.CROSSBOW_HOLD;
-            }
-            return HumanoidModel.ArmPose.ITEM;
-        }
     }
 }
